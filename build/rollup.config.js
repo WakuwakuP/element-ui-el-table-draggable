1e0af32ce1a8a373b089677e4c8a32d461b4ab30
WakuwakuP <naoki.fujisawa@wakuwakup.net>
2019-05-08 12:14:38 +0900

    rollup

--- build/rollup.config.js
+++ build/rollup.config.js
@@ -1,4 +1,5 @@
 import vue from 'rollup-plugin-vue'; // .vue 単一ファイルコンポーネントを取得
+import commonjs from 'rollup-plugin-commonjs';
 import buble from 'rollup-plugin-buble'; // 適切にブラウザをサポートするトランスパイラおよびポリフィル
 export default {
     input: 'src/wrapper.js', // Path relative to package.json
@@ -7,9 +8,10 @@ export default {
         exports: 'named',
     },
     plugins: [
+        commonjs(),
         vue({
-            css: true, // css を <style> タグとして注入
-            compileTemplate: true, // 明示的にテンプレートを描画関数に変換
+          css: true, // css を <style> タグとして注入
+          compileTemplate: true, // 明示的にテンプレートを描画関数に変換
         }),
         buble(), // ES5 へトランスパイルする
     ],
